1276bdac2c166758858135201eff67a5afc2a33c
zachbaker55 <thezachbaker55@gmail.com>
2022-11-01 20:02:08 -0400

    Finished 5b

--- Global.gd
+++ Global.gd
@@ -1,5 +1,16 @@
 extends Node
 
+func _ready():
+	pause_mode = PAUSE_MODE_PROCESS		# global should never be paused
+
 func _unhandled_input(event):
-	if event.is_action_pressed("menu"):
-		get_tree().quit()
+	if event.is_action_pressed("menu"):	# instead of quitting, show the menu
+		#get_tree().quit()
+		var menu = get_node_or_null("/root/Game/Menu")
+		if menu != null:
+			if not menu.visible:
+				menu.show()
+				get_tree().paused = true 	# pause the game while the menu is visible
+			else:
+				menu.hide()
+				get_tree().paused = false
